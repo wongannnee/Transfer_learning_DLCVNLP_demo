1f9ac9b35903f1ffb35f67969f796bdb4e083141
c17hawke <sunny.c17hawke@gmail.com>
2021-11-20 18:02:54 +0530

    req and setup updated

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 ## edit below variables as per your requirements -
-REPO_NAME = "REPO_NAME"
+REPO_NAME = "Tranfer_learning_DLCVNL_demo"
 AUTHOR_USER_NAME = "c17hawke"
 SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = []
@@ -14,7 +14,7 @@ setup(
     name=SRC_REPO,
     version="0.0.1",
     author=AUTHOR_USER_NAME,
-    description="Template for python project",
+    description="Trasnfer learning demo for DLCVNLP Aug",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
